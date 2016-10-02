c976faceffcaf7c25feae4a558fe531bb4186824
Perrywzp <wangzhipei1121@163.com>
2016-10-02 15:45:38 +0800

    [gallery] 提交画廊结构代码

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,41 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
+// 获取图片相关的数据
+let imageDatas = require('../data/imageData.json');
+// 利用自执行函数，将图片信息转成图片URL路径信息；
+imageDatas = (function getImageURL(imageDatasArr){
+    var singleImageData;
+    for(let i=0, j = imageDatasArr.length; i < j; i++){
+        singleImageData=  imageDatasArr[i];
 
-let yeomanImage = require('../images/yeoman.png');
+        singleImageData.imageURL = require('../images/' + singleImageData.fileName);
 
-class AppComponent extends React.Component {
+        imageDatasArr[i] = singleImageData.imageURL;
+    }
+
+    return imageDatasArr;
+})(imageDatas);
+
+
+
+class GalleryByReactApp extends React.Component {
   render() {
-    return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
-    );
+      return (
+          <section className="stage">
+              <section className="img-sec">
+
+              </section>
+              <nav className="controller-nav">
+
+              </nav>
+          </section>
+      )
   }
 }
 
-AppComponent.defaultProps = {
+GalleryByReactApp.defaultProps = {
 };
 
-export default AppComponent;
+export default GalleryByReactApp;
