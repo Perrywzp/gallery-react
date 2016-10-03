837a4e5ac337424fcccde341af8348b2fca734f5
Perrywzp <wangzhipei1121@163.com>
2016-10-03 21:49:27 +0800

    [gallery] 实现画廊的控制功能

--- src/components/Main.js
+++ src/components/Main.js
@@ -51,8 +51,8 @@ class ImgFigure extends React.Component {
 
         // 如果图片的旋转角度有值，且不为0，添加旋转角度
         if (this.props.arrange.rotate) {
-            ['-moz-', '-ms-', '-webkit-', ''].forEach(function (value) {
-                styleObj[value + 'transform'] = 'rotate(' + this.props.arrange.rotate + 'deg)';
+            ['MozTransform', 'msTransform', 'WebkitTransform', 'transform'].forEach(function (value) {
+                styleObj[value] = 'rotate(' + this.props.arrange.rotate + 'deg)';
             }.bind(this));
 
         }
@@ -78,6 +78,42 @@ class ImgFigure extends React.Component {
     }
 }
 
+
+class ControllerUnit extends React.Component{
+
+    handleClick(e){
+
+        // 如果点击的是当前正在选中态的按钮，则翻转图片，否则将对应的图片居中
+        if(this.props.arrange.isCenter){
+            this.props.inverse()
+        }else{
+            this.props.center()
+        }
+
+        e.stopPropagation();
+        e.preventDefault();
+    }
+    render() {
+
+        var controllerUnitClassName = 'controller-unit';
+
+        // 如果对应的是居中的图片，显示控制按钮的居中态
+        if(this.props.arrange.isCenter){
+            controllerUnitClassName += ' is-center';
+
+            //如果同时对应的是翻转图片，显示控制按钮的翻转态
+            if(this.props.arrange.isInverse){
+                controllerUnitClassName += ' is-inverse';
+            }
+        }
+
+        return (
+            <span className={controllerUnitClassName} onClick={this.handleClick.bind(this)}>
+
+            </span>
+        );
+    }
+}
 
 class GalleryByReactApp extends React.Component {
 
@@ -115,7 +151,7 @@ class GalleryByReactApp extends React.Component {
             vPosRangeX = vPosRange.x,
 
             imgsArrangeTopArr = [],
-            topImgNum = Math.ceil(Math.random() * 2), // 取一个或者不取
+            topImgNum = Math.floor(Math.random() * 2), // 取一个或者不取
             topImgSpliceIndex = 0,
             imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex, 1);
 
@@ -144,6 +180,8 @@ class GalleryByReactApp extends React.Component {
                 isCenter: false
             }
         });
+
+
 
         // 布局左右两侧的图片
         for (let i = 0, j = imgsArrangeArr.length, k = j / 2; i < j; i++) {
@@ -263,6 +301,13 @@ class GalleryByReactApp extends React.Component {
             imgFigures.push(<ImgFigure
                 key={value.imageURL} data={value}
                 ref={'imgFigure' + index}
+                arrange={this.state.imgsArrangeArr[index]}
+                inverse={this.inverse(index)}
+                center={this.center(index)}
+            />);
+
+            controllerUnits.push(<ControllerUnit
+                key={'controllerIndex'+index}
                 arrange={this.state.imgsArrangeArr[index]}
                 inverse={this.inverse(index)}
                 center={this.center(index)}
